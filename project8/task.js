0087c5800dfdfb553fef67f4c851863886df55a5
Денис Лысюк <denyslysiuk@MacBook-Pro-Denis.local>
2020-03-04 15:11:08 +0200

    project8

---
+++ project8/task.js
@@ -0,0 +1,18 @@
+// 6, 7, 8, 9, ... , 15
+
+// function rangeSum(start, end) {
+//     let range =[start];
+        
+//     }
+// }
+
+// for (let n = 0; n < 10; n++) {
+//     console.log = 10[n];
+    
+// }
+
+if (n===1) {
+    return 1
+}
+
+
